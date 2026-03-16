035d371853810bef8a2dec2549e0a1e770bde01c
md junaid qamar <junaidqamar8753@gmil.com>
2026-03-16 23:10:11 +0530

    anav added

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/nav.css';
+
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+      <div className="nav-links">
+        <li >Home</li>
+        <li >About</li>
+        <li >Projects</li>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
